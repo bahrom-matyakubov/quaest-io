1d432ce3174e9d52a32d4bdee68cfa714c3a3210
Bahrom Matyakubov <bahrom.matyakubov@gmail.com>
2016-01-02 18:32:21 -0500

    Adding more questions view drafts

--- quaestio/questions/views.py
+++ quaestio/questions/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world, you are at questions index!")
-# Create your views here.
+    latest_questions = Question.objects.order_by('-created')[:5]
+    template = loader.get_template('questions/index.html')
+    context = {
+        'latest_questions': latest_questions,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
