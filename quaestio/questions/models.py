955badbd443f796e195c2911e672f6b696efe999
Bahrom Matyakubov <bahrom.matyakubov@gmail.com>
2016-01-02 13:25:52 -0500

    Initial question layout

---
+++ quaestio/questions/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Post(models.Model):
+    # TODO: Add rating
+    # TODO: Link author with a user
+    content = models.CharField(max_length=500)
+    author = models.CharField(max_length=20)
+    created = models.DateTimeField('Created')
+    edited = models.DateTimeField('Edited')
+
+    class Meta:
+        abstract = True
+
+
+class Tag(models.Model):
+    # TODO: Sort out tag creation, author, etc.
+    name = models.CharField(max_length=20)
+
+
+class Question(Post):
+    # TODO: Question rating works the same as answer rating?
+    tags = models.ManyToManyField(Tag, verbose_name='List of tags')
+
+
+class Answer(Post):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
