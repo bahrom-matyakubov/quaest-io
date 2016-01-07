313431935a9e48150ddc5b4a4d6192c0b7aa00b3
Bahrom Matyakubov <bahrom.matyakubov@gmail.com>
2016-01-07 00:24:31 -0500

    Adding api endpoints for accountviewset

--- quaestio/quaestio/urls.py
+++ quaestio/quaestio/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+from .views import IndexView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
+    '',
+    url(r'^api/v1/', include(router.urls)),
     url(r'^questions/', include('questions.urls')),
     url(r'^admin/', admin.site.urls),
+    url('^.*$', IndexView.as_view(), name='index'),
 ]
